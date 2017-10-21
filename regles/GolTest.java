d1bc9cd4e68512aaf8ff1f880ea3734bad126356
Emmanuel Aron <emmanuel.aron@gmail.com>
2017-10-21 02:01:15 +0200

    4 voisins

--- regles/GolTest.java
+++ regles/GolTest.java
@@ -1,3 +1,4 @@
+package regles;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,41 +13,44 @@ public class GolTest {
 	Gol gol;
 	
 	/**
-	 * -peuplé:
-	 * chaque cellule avec 1 ou 0 voisins meurt.(par solitude)
-	 * chaque cellule avec 4 ou plus voisins meurt (surpopulation)
-	 * chaque cellule avec 2 ou 3 voisins survit
+	 * -peuplee:
+	 * chaque cellule vivante avec 1 ou 0 voisins meurt.(par solitude)
+	 * chaque cellule vivante avec 4 voisins ou plus meurt (surpopulation)
+	 * chaque cellule vivante avec 2 ou 3 voisins survit
 	 * -vide:
 	 * chaque cellule vide avec 3 voisins se peuple.
 	 * 
 	 */
 	
-	
-	
 	@Test
-	public void cellulePeuplé_avec_0_ou_1_voisin_meurt(){
+	public void cellulePeuple_avec_0_ou_1_voisin_meurt(){
 		
 		cell = new Cellule(true);
 		gol = new Gol(cell);
 		assertEquals(gol.etatNouvellePhase(0,true), false);
 		assertEquals(gol.etatNouvellePhase(1,true), false);
 	}
-	
-	/*@Test
-	public void celluleVide_avec_3_voisins-se_peuple(){
-		
-	}*/
-	
+	@Test
+	public void cellulePeuple_avec_4_voisins_ou_plus_meurt(){
+		cell = new Cellule(true);
+		gol = new Gol(cell);
+		assertEquals(gol.etatNouvellePhase(4,true), false);
+	}
 	
 	@Test
-	public void cellulePeuplé_avec_2_ou_3_voisin_survit(){
+	public void celluleVideAvec3voisinsSePeuple(){
+		cell = new Cellule(false);
+		gol = new Gol(cell);
+		assertEquals(gol.etatNouvellePhase(3,false), true);
+	}
+	
+	@Test
+	public void cellulePeuple_avec_2_ou_3_voisin_survit(){
 		
 		cell = new Cellule(true);
 		gol = new Gol(cell);
 		assertEquals(gol.etatNouvellePhase(2,true), true);
 		assertEquals(gol.etatNouvellePhase(3,true), true);
 	}
-	
-	
 	
 }
